2e989cc1a30704e76fe6a1f37ba525d444b7bbdb
Can Aknesil <aknesil@kth.se>
2021-06-13 01:11:18 +0200

    Transition to jquery completed.

--- js/view.js
+++ js/view.js
@@ -12,71 +12,34 @@ var listener = null; // Set by controller.
 
 
 // Draw chessboard
-// (function() {
-//     var board_svg = $(document.createElementNS(svg_ns, "svg")).attr(
-// 	{'viewBox': '0 0 800 800',
-// 	 'id': 'chessboard-svg'}).appendTo('#board');
-
-//     for (var x=0; x<8; x++) {
-// 	for (var y=0; y<8; y++) {
-// 	    var file = String.fromCharCode(x + 'a'.charCodeAt());
-// 	    var rank = String.fromCharCode(7 - y + '1'.charCodeAt());
-// 	    var id = file + rank;
-	    
-// 	    var square = $(document.createElementNS(svg_ns, "rect")).attr(
-// 		{'width': '100',
-// 		 'height': '100',
-// 		 'x': x * 100,
-// 		 'y': y * 100,
-// 		 'id': id})
-// 		.appendTo(board_svg)
-// 		.click(function() { square_onclick(this.id); });
-	    
-// 	    if ((x + y) % 2 != 0)
-// 		square.addClass('square-dark');
-// 	    else
-// 		square.addClass('square-light');
-	    
-// 	    squares[x][7 - y] = square;
-// 	}
-//     }
-    
-// })();
-
 (function() {
-    var board = document.getElementById("board");
-    board_svg = document.createElementNS(svg_ns, "svg");
-    
-    board_svg.setAttribute("viewBox", "0 0 800 800");
-    board_svg.setAttribute("id", "chessboard-svg");
-    
+    board_svg = $(document.createElementNS(svg_ns, "svg")).attr(
+	{'viewBox': '0 0 800 800',
+	 'id': 'chessboard-svg'}).appendTo('#board');
+
     for (var x=0; x<8; x++) {
 	for (var y=0; y<8; y++) {
-	    var square = document.createElementNS(svg_ns, "rect");
-	    squares[x][7 - y] = square;
-	    
-	    square.setAttribute("width", "100");
-	    square.setAttribute("height", "100");
-	    square.setAttribute("x", x * 100);
-	    square.setAttribute("y", y * 100);
-	    
-	    if ((x + y) % 2 != 0)
-		square.classList.add("square-dark");
-	    else
-		square.classList.add("square-light");
-	    
 	    var file = String.fromCharCode(x + 'a'.charCodeAt());
 	    var rank = String.fromCharCode(7 - y + '1'.charCodeAt());
 	    var id = file + rank;
-	    square.setAttribute("id", id);
-
-	    square.addEventListener("click", function() {square_onclick(this.id);});
 	    
-	    board_svg.appendChild(square);
+	    var square = $(document.createElementNS(svg_ns, "rect"))
+		.attr({'width': '100',
+		       'height': '100',
+		       'x': x * 100,
+		       'y': y * 100,
+		       'id': id})
+		.appendTo(board_svg)
+		.click(function() { square_onclick(this.id); });
+	    
+	    if ((x + y) % 2 != 0)
+		square.addClass('square-dark');
+	    else
+		square.addClass('square-light');
+	    
+	    squares[x][7 - y] = square;
 	}
-    }
-    
-    board.appendChild(board_svg);
+    }    
 })();
 
 
@@ -106,18 +69,16 @@ function toggle_orientation() {
 
 
 function new_piece(piece_type, x, y) {
-    var img = document.createElementNS(svg_ns, "image");
-    img.setAttribute("href", "/images/pieces/classic/" + piece_type + ".svg");
-    img.setAttribute("x", 4 * 100); // Temporary. Will be changed immediately after creation.
-    img.setAttribute("y", 4 * 100);
-    img.setAttribute("height", "100");
-    img.setAttribute("width", "100");
-    
-    img.classList.add("piece");
-    img.classList.add(piece_type);
-    img.setAttribute("piece_type", piece_type);
+    var img = $(document.createElementNS(svg_ns, "image"))
+	.attr({"href": "/images/pieces/classic/" + piece_type + ".svg",
+	       'x': 4 * 100,
+	       'y': 4 * 100,
+	       'height': '100',
+	       'width': '100',
+	       'piece_type': piece_type,
+	       'class': "piece " + piece_type})
+	.appendTo(board_svg);
 
-    board_svg.appendChild(img);
     move_piece(img, x, y, true, false);
     position[x][y] = img;
 }
@@ -137,31 +98,29 @@ function move_piece(piece, x, y, remove=true, animation=true, post_animation_cb=
     var old_piece = position[x][y];
     var [board_x, board_y] = board_coordinates(x, y);
     var square_size = 100;
-    if (animation) {
-	$(piece).velocity(
-	    {x: board_x * square_size,
-	     y: (7 - board_y) * square_size},
-	    {duration: 400
-	     ,complete: function(elements, activeCall) {
-		 if (remove) {
-		     if (old_piece != null) {
-			 old_piece.remove();
-		     }
-		 }
-		 if (post_animation_cb)
-		     post_animation_cb();
-	     }
-	    });
-    } else {
-	piece.setAttribute("x", board_x * square_size);
-	piece.setAttribute("y", (7 - board_y) * square_size);
+
+    var new_x = board_x * square_size;
+    var new_y = (7 - board_y) * square_size;
+
+    function post_func() {
 	if (remove) {
 	    if (old_piece != null) {
-		old_piece.remove();
+		$(old_piece).remove();
 	    }
 	}
 	if (post_animation_cb)
 	    post_animation_cb();
+    }
+    
+    if (animation) {
+	$(piece).velocity(
+	    {x: new_x, y: new_y},
+	    {duration: 400
+	     ,complete: function(elements, activeCall) { post_func(); }
+	    });
+    } else {
+	$(piece).attr({'x': new_x, 'y': new_y});
+	post_func();
     }
 }
 
@@ -185,7 +144,7 @@ function update_from_position(new_position) {
 	    var square_name = Util.position_index_to_name(i, j);
 	    var piece_type = Util.position_get_piece_by_index(new_position, i, j);
 	    var gui_piece = position[i][j];
-	    var gui_piece_type = (gui_piece == null ? null : gui_piece.getAttribute("piece_type"));
+	    var gui_piece_type = (gui_piece == null ? null : gui_piece.attr("piece_type"));
 	    if (gui_piece_type != piece_type) {
 		if (piece_type == null)
 		    remove_piece(i, j);
@@ -205,19 +164,19 @@ function get_square_element(x, y) {
 
 function mark_square(x, y) {
     var square = get_square_element(x, y);
-    square.classList.add("marked");
+    square.addClass("marked");
     markings[x][y] = true;
 }
 
 function unmark_square(x, y) {
     var square = get_square_element(x, y);
-    square.classList.remove("marked");
+    square.removeClass("marked");
     markings[x][y] = false;
 }
 
 function is_square_marked(x, y) {
     var square = get_square_element(x, y);
-    var is_marked_square = square.classList.contains("marked");
+    var is_marked_square = square.hasClass("marked");
     var is_marked_store = markings[x][y];
     console.assert(is_marked_square == is_marked_store);
     return is_marked_store;
